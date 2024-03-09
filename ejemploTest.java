22b06c4bf9ecab0a02bf5722efd1a4ce92ac5ab7
fraanrubioo <facisco199959@gmail.com>
2024-03-09 21:50:13 +0100

    Pruebas

--- ejemploTest.java
+++ ejemploTest.java
@@ -1,19 +1,29 @@
+
 import com.mycompany.piedrapapelytijera.PiedraPapelyTijera;
+import org.junit.jupiter.api.Assertions;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
 
 
-  Click nbfsnbhostSystemFileSystemTemplatesLicenseslicense-default.txt to change this license
-  Click nbfsnbhostSystemFileSystemTemplatesUnitTestsJUnit5TestClass.java to edit this template
- 
-
-
-
- 
-  @author usuario
- 
+/**
+ *
+ * @author usuario
+ */
 public class PiedraPapelyTijeraTest {
     
     public void TestJuego() {
         PiedraPapelyTijera juego = new PiedraPapelyTijera();
         juego.jugar();
+        
+        Assertions.assertEquals("piedra","piedra");
+        juego.jugar();
+        Assertions.assertEquals("piedra","papel");
+        juego.jugar();
+        Assertions.assertEquals("piedra","tijera");
+        juego.jugar();
+        
     }
 }
