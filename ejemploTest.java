e5bf1c5db5a7206f97afc9c3c00eeab95c890a91
fraanrubioo <facisco199959@gmail.com>
2024-03-09 21:33:28 +0100

    Junit test

---
+++ ejemploTest.java
@@ -0,0 +1,19 @@
+import com.mycompany.piedrapapelytijera.PiedraPapelyTijera;
+
+
+  Click nbfsnbhostSystemFileSystemTemplatesLicenseslicense-default.txt to change this license
+  Click nbfsnbhostSystemFileSystemTemplatesUnitTestsJUnit5TestClass.java to edit this template
+ 
+
+
+
+ 
+  @author usuario
+ 
+public class PiedraPapelyTijeraTest {
+    
+    public void TestJuego() {
+        PiedraPapelyTijera juego = new PiedraPapelyTijera();
+        juego.jugar();
+    }
+}
